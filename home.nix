ac929c12d10c64e72fc416f6a45a086e829f5d91
Rob Eerkes <rreerkes@hotmail.com>
2026-01-17 11:13:26 +0100

    Initial commit

---
+++ home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "rob";
+  home.homeDirectory = "/home/rob";
+  programs.git.enable = true;
+  home.stateVersion = "25.11";
+  programs.bash = {
+    enable = true;
+  };
+
+  programs.git = {
+    settings = {
+      user = {
+        name = "Rob Eerkes";
+	email = "rreerkes@hotmail.com";
+      };
+      init.defaultBranch = "master";
+};
+};
+  
+  home.file.".config/nvim".source = ./config/nvim;
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+
+  ];
+
+
+}
