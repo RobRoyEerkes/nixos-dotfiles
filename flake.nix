e1f354e0884e592f951ee8337b268b885dc26861
Rob Eerkes <rreerkes@hotmail.com>
2026-01-18 20:00:40 +0100

    cleaned up the tree

--- flake.nix
+++ flake.nix
@@ -1,33 +1,39 @@
 {
-	description = "NixOS from scratch";
-	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-25.11";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.11";
-			inputs.nixpkgs.follows = "nixpkgs";
+  description = "NixOS from scratch";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
 
-		};
-		nixvim.url = "github:RobRoyEerkes/nvim-config";
+    };
+    nixvim.url = "github:RobRoyEerkes/nvim-config";
 
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.nixos-rob = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.rob = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
 
-	};
-	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-		nixosConfigurations.nixos-rob = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./configuration.nix
-				home-manager.nixosModules.home-manager 
-				{
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.rob = import ./home.nix;
-						backupFileExtension = "backup";
-					};
-				}
-				
-			];
-                        specialArgs = { inherit inputs; };
-		};
-	};
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
 }
