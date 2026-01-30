c9aa07d172839fce953932883c8e2f889c430082
Rob Eerkes <rreerkes@hotmail.com>
2026-01-30 01:11:48 +0100

    changed init.lua and begon to seperate that

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -125,6 +125,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local Snacks = require("snacks")
+		if #vim.fn.argv() == 0 then
+			-- Only show dashboard if no files are passed on startup
+			Snacks.dashboard.open()
+		end
+	end,
+})
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -188,17 +197,41 @@ require("lazy").setup({
 			},
 		},
 	},
-
 	{
-		"folke/snacks.nvim",
-		---@type snacks.Config
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+		},
 		opts = {
-			explorer = {
-				enable = true,
-				replace_netrw = true,
+			options = {
+				always_show_bufferline = false,
+				offsets = {
+					{
+						filetype = "snacks_explorer",
+						text = "Explorer",
+						highlight = "Directory",
+						text_align = "left",
+					},
+					{
+						filetype = "snacks_layout_box",
+					},
+				},
 			},
 		},
 	},
+	require("plugins.snacks"),
+
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 	--
 	-- This is often very useful to both group configuration, as well as handle
