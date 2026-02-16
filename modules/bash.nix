5616815611ca5dfe1ed178d9e93cac21ec7c01bd
Rob Eerkes <rreerkes@hotmail.com>
2026-02-16 23:32:49 +0100

    removed a few eval warnings

--- modules/bash.nix
+++ modules/bash.nix
@@ -4,9 +4,16 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nixos-switch = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#${hostname}";
+      nixos-switch = "sudo nixos-rebuild switch -p test --flake ~/nixos-dotfiles#${hostname}";
+      nixos-switch-stable = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#${hostname}";
       nixos-test = "sudo nixos-rebuild test --flake ~/nixos-dotfiles#${hostname}";
+      nixos-upgrade = "nix flake update --flake ~/nixos-dotfiles && sudo nixos-rebuild switch --upgrade --flake ~/nixos-dotfiles#${hostname}";
+
+      ls = "eza";
+      cat = "bat";
+      cd = "zoxide";
     };
+    initExtra = "fastfetch";
   };
 
   programs.starship = {
--- modules/home.nix
+++ modules/home.nix
@@ -30,7 +30,7 @@
       # https://github.com/nix-community/home-manager/issues/632
       program_options = {
         # replace with your favorite file manager
-        file_manager = "${pkgs.xfce.thunar}/bin/thunar";
+        file_manager = "${pkgs.thunar}/bin/thunar";
       };
     };
   };
