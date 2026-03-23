4055e8635d781cfd955453db0a2d7f2c227e4bc7
Rob Eerkes <rreerkes@hotmail.com>
2026-03-23 16:03:01 +0100

    added obs

--- modules/games.nix
+++ modules/games.nix
@@ -8,9 +8,21 @@
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
   };
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
   environment.systemPackages = with pkgs; [
     godot
     # krita
+
     blender
   ];
 }
