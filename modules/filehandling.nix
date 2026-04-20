878fd0a3b34f266392b18fe3625400ed13bfabf3
Rob Eerkes <rreerkes@hotmail.com>
2026-04-20 22:50:40 +0200

    added archiving to thunar

--- modules/filehandling.nix
+++ modules/filehandling.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    thunar
     ristretto
     tumbler
     gimp
+    file-roller
   ];
+
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+    ];
+  };
+
   services.gvfs.enable = true;
   programs.xfconf.enable = true;
   programs.dconf.enable = true;
