18b0f7fd481ae11e166f6d28d0b69d6dc0166e82
Rob Eerkes <rreerkes@hotmail.com>
2026-04-30 16:30:40 +0200

    more sound options added (still don't know what they are used for except jack

--- modules/default.nix
+++ modules/default.nix
@@ -40,9 +40,13 @@
 
   services.dbus.enable = true;
 
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
   services.power-profiles-daemon.enable = true;
