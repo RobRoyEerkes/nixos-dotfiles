3ad00891b2046bea9b673a92a68dd75b65c76607
Rob Eerkes <rreerkes@hotmail.com>
2026-05-10 13:05:14 +0200

    added some diagnostics

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -26,6 +26,15 @@
       };
       register = "unnamedplus";
     };
+    diagnostics = {
+      virtual_text = {
+        spacing = 4;
+        prefix = "●";
+      };
+      underline = true;
+      # Disable updates while typing so it only refreshes on save/exit insert
+      update_in_insert = false;
+    };
 
     colorschemes.tokyonight.enable = true;
 
@@ -142,6 +151,36 @@
         };
       };
 
+      # trouble = {
+      #   enable = true;
+      #   settings = {
+      #     modes = {
+      #       diagnostics = {
+      #         # Sort by Error, then Warning, etc.
+      #         sort = [
+      #           "severity"
+      #           "pos"
+      #         ];
+      #         # Filter to only show errors from the current file by default
+      #         filter = {
+      #           buf = 0;
+      #         };
+      #         mode = "diagnostics";
+      #       };
+      #
+      #       # You can even create a custom "preview" mode
+      #       preview = {
+      #         type = "float";
+      #         relative = "editor";
+      #         border = "rounded";
+      #       };
+      #       keys = {
+      #         "P" = "preview";
+      #       };
+      #     };
+      #   };
+      # };
+
       render-markdown.enable = true;
       bufferline.enable = true;
       lualine.enable = true;
@@ -159,6 +198,18 @@
         };
       };
     };
+    autoCmd = [
+      {
+        event = [ "BufWritePost" ];
+        callback = {
+          __raw = ''
+            function()
+              vim.diagnostic.show()
+            end
+          '';
+        };
+      }
+    ];
 
     keymaps = [
       {
@@ -191,6 +242,44 @@
         action = "<CMD>silent !zathura %:r.pdf &<CR>";
         options.desc = "View Pdf";
       }
+      {
+        mode = "n";
+        key = "<leader>xx";
+        action = "<CMD>lua vim.diagnostic.show() <CR>";
+
+        options.desc = "Open floating diagnostic message";
+      }
+      {
+        mode = "n";
+        key = "<leader>xX";
+        action = "<CMD>lua vim.diagnostic.open_float() <CR>";
+
+        options.desc = "Open floating diagnostic message";
+      }
+      # {
+      #   mode = "n";
+      #   key = "<leader>xx";
+      #   action = "<cmd>Trouble diagnostics toggle<cr>";
+      #   options.desc = "Diagnostics (Trouble)";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<leader>xX";
+      #   action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+      #   options.desc = "Buffer Diagnostics (Trouble)";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<leader>cs";
+      #   action = "<cmd>Trouble symbols toggle focus=false<cr>";
+      #   options.desc = "Symbols (Trouble)";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<leader>cl";
+      #   action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+      #   options.desc = "LSP Definitions / references / ... (Trouble)";
+      # }
     ];
 
     globals = {
