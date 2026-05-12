98f79dbe8eee42e853095f9b4e5ee91d5dfbbc2f
Rob Eerkes <rreerkes@hotmail.com>
2026-05-12 19:06:46 +0200

    Updated system

--- modules/openrgb.nix
+++ modules/openrgb.nix
@@ -3,11 +3,12 @@
   hardware.i2c.enable = true;
   services.hardware.openrgb = {
     package = pkgs.openrgb.overrideAttrs (old: {
+      # TODO: make this a commit not the master branch for less frequent updates
       src = pkgs.fetchFromGitLab {
         owner = "CalcProgrammer1";
         repo = "OpenRGB";
         rev = "master";
-        sha256 = "sha256-/a6+UQPkdVACoIyexMHENsAPA7xxLmx/qhohGFB2MFw=";
+        sha256 = "sha256-1Tnw45J9Hm9Va9KQpcFUMS8UCQrJNIc3KeKYvDVch/4=";
       };
       patches = [ ];
       version = "experimental";
